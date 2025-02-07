d051a97e4f1f5f667ea8580bda1461fa8613c110
jonatas1n <jonatasgeek@gmail.com>
2025-02-07 19:55:48 -0300

    chore: set usage of env file

--- database/db.py
+++ database/db.py
@@ -5,10 +5,16 @@ from .base import Base
 from experiences.models import *
 from projects.models import *
 from skills.models import *
+import os
 
-engine = create_engine(
-    "mysql+mysqlconnector://root:password@jonatas1n-desktop/portfolio"
-)
+DATABASE_HOST = os.getenv("DATABASE_HOST", "jonatas1n-desktop")
+DATABASE_USER = os.getenv("DATABASE_USER", "root")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "portfolio")
+
+DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+engine = create_engine(DATABASE_URL)
 
 with engine.connect() as conn:
     conn.execute(text("CREATE DATABASE IF NOT EXISTS portfolio"))
