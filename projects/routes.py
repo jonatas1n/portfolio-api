f27c90d7ce2c954df79f7a0b6f687468f4ee3624
jonatas1n <jonatasgeek@gmail.com>
2025-02-11 01:16:18 -0300

    feat: create a route to list projects tecnologies

--- projects/routes.py
+++ projects/routes.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .controller import list_projects
+from .controller import list_projects, list_projects_technologies
 from database.db import get_db
 
 router = APIRouter(prefix="/projects", tags=["projects"])
 
 @router.get("/")
-def list(db: Session = Depends(get_db)):
-    return list_projects(db)
+def list(db: Session = Depends(get_db), technologies: list[str] = []):
+    return list_projects(db, technologies)
+
+@router.get("/technologies")
+def technologies(db: Session = Depends(get_db)):
+    technologies_list = list_projects_technologies(db)
+    return {"technologies": technologies_list}
