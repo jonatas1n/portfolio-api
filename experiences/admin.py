6be4a84fdd821026d64738e13a6f23a866903128
jonatas1n <jonatasgeek@gmail.com>
2025-02-11 01:16:47 -0300

    chore: update admin file

--- experiences/admin.py
+++ experiences/admin.py
@@ -1,10 +1,11 @@
 from sqladmin import ModelView
 from .models import Experiences
+import json
 
 class ExperienceAdmin(ModelView, model=Experiences):
     column_list = [
         Experiences.id, 
-        Experiences.title, 
+        Experiences.position, 
         Experiences.company_name, 
         Experiences.start_date, 
         Experiences.end_date, 
@@ -12,13 +13,13 @@ class ExperienceAdmin(ModelView, model=Experiences):
         Experiences.technologies
     ]
     
-    column_searchable_list = [Experiences.title, Experiences.company_name]
+    column_searchable_list = [Experiences.position, Experiences.company_name]
     column_sortable_list = [Experiences.start_date, Experiences.end_date]
 
-    def on_model_change(self, form, model, is_created):
-        if isinstance(model.technologies, list):
-            model.technologies = ",".join(model.technologies)
-
-    def on_model_delete(self, model):
-        if model.technologies == "":
-            model.technologies = None
+    async def on_model_get(self, model):
+        if isinstance(model.technologies, str):
+            try:
+                model.technologies = json.loads(model.technologies)
+            except json.JSONDecodeError:
+                model.technologies = []
+                
