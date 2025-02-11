e330f9996e5d4994b3bcdab89da0302a37f7603d
jonatas1n <jonatasgeek@gmail.com>
2025-02-11 01:17:08 -0300

    feat: create list experiences technologies controller function

--- experiences/controller.py
+++ experiences/controller.py
@@ -2,6 +2,27 @@ from sqlalchemy.orm import Session
 from .models import Experiences
 from database.db import get_db
 from fastapi import Depends
+import json
 
-def list_experiences(db: Session = Depends(get_db)):
+def list_experiences(db: Session = Depends(get_db), technologies: list[str] = []):
+    if technologies:
+        return db.query(Experiences).filter(
+            Experiences.technologies.overlap(technologies)
+        ).all()
     return db.query(Experiences).all()
+
+def list_experiences_technologies(db: Session = Depends(get_db)):
+    result = db.query(Experiences.technologies).all()
+
+    if not result:
+        return []
+    
+    parsed_technologies = []
+
+    for item in result:
+        item_list = json.loads(item[0])
+        print(type(item_list))
+        parsed_technologies += item_list
+
+    parsed_technologies = list(set(parsed_technologies))
+    return parsed_technologies
