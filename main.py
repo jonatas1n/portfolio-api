f27cc18373c55d7dd854cbd306ff84bd6dff2483
jonatas1n <jonatasgeek@gmail.com>
2025-02-11 01:14:47 -0300

    feat: add cors treatment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from admin import init_all_admins
 
 from skills.routes import router as skills_router
@@ -6,6 +7,13 @@ from projects.routes import router as projects_router
 from experiences.routes import router as experiences_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_all_admins(app)
 
