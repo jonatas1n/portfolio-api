2fccd8160fedea6bc2088669edec984069087a7f
jonatas1n <jonatasgeek@gmail.com>
2025-02-11 01:15:38 -0300

    feat: create list projects technologies

--- projects/controller.py
+++ projects/controller.py
@@ -3,5 +3,24 @@ from .models import Projects
 from database.db import get_db
 from fastapi import Depends
 
-def list_projects(db: Session = Depends(get_db)):
+def list_projects(db: Session = Depends(get_db), technologies: list[str] = []):
+    if technologies:
+        return db.query(Projects).filter(
+            Projects.technologies.overlap(technologies)
+        ).all()
     return db.query(Projects).all()
+
+def list_projects_technologies(db: Session = Depends(get_db)):
+    result = db.query(Projects.technologies).all()
+
+    if not result:
+        return []
+
+    parsed_technologies = []
+
+    for item in result:
+        item_list = item[0]
+        parsed_technologies += item_list
+
+    parsed_technologies = list(set(parsed_technologies))
+    return parsed_technologies
