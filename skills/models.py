6ba322fc6dcd180b775964e033df662ff849d76f
jonatas1n <jonatasgeek@gmail.com>
2025-02-16 04:14:55 -0300

    feat: add english columns to table

--- skills/models.py
+++ skills/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Text
 from database.base import Base
 
 
@@ -8,4 +8,5 @@ class Skills(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(30), unique=True)
-    description: Mapped[str] = mapped_column(String(2048), nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    description_en: Mapped[str] = mapped_column(Text, nullable=True)
