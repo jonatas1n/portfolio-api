aab0494db0a864af487e01dca962e5c175df86bb
jonatas1n <jonatasgeek@gmail.com>
2025-02-18 12:32:51 -0300

    fix: remove typo

--- database/db.py
+++ database/db.py
@@ -11,7 +11,6 @@ DATABASE_HOST = os.getenv("DATABASE_HOST", "jonatas1n-desktop")
 DATABASE_USER = os.getenv("DATABASE_USER", "root")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "portfolio")
-jonatas1n.mysql.pythonanywhere-services.com
 
 DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
