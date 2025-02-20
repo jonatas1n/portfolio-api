3d196c27f564d6111edde45c2848727e0345c9b1
jonatas1n <jonatasgeek@gmail.com>
2025-02-20 01:59:44 -0300

    chore: update certifcate reading

--- database/db.py
+++ database/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+import ssl
 
 from .base import Base
 from experiences.models import *
@@ -12,13 +13,17 @@ DATABASE_USER = os.getenv("DATABASE_USER", "")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "")
 DATABASE_PORT = os.getenv("DATABASE_PORT", "16604")
-CA_CERTIFICATE = os.getenv("CA_CERTIFICATE")
+CA_CERTIFICATE = os.getenv("CA_CERTIFICATE", "").replace("  ", "\n")
+
 
 DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
+ssl_context = ssl.create_default_context()
+ssl_context.load_verify_locations(cadata=CA_CERTIFICATE)
+
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"ssl": {"ca": CA_CERTIFICATE}}
+    connect_args={"ssl": ssl_context}
 )
 
 with engine.connect() as conn:
