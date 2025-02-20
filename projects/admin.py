3f6ad26f25167e665c952779e41cacf68daebb07
jonatas1n <jonatasgeek@gmail.com>
2025-02-20 02:09:46 -0300

    chore: remove write type dirs functions

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,6 @@
 from sqladmin import ModelView
-from starlette.requests import Request
 from .models import Projects
-import shutil
 import os
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 
 class ProjectAdmin(ModelView, model=Projects):
     edit_template = "custom_edit.html"
@@ -25,26 +19,6 @@ class ProjectAdmin(ModelView, model=Projects):
 
     form_columns = ["title", "technologies", "description", "images"]
 
-    async def on_model_change(self, form, model, is_created, request: Request):
-        form_data = await request.form()
-        file = form_data.get("images")
-
-        if file and hasattr(file, "filename"):
-            file_path = f"{UPLOAD_FOLDER}/{file.filename}"
-            with open(file_path, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
-
-            model.images = file_path
-
     async def on_model_delete(self, model):
         if model.images and os.path.exists(model.images):
             os.remove(model.images)
-
-    def _format_image(self, model):
-        print(model)
-        return "Sem imagem"
-        if model.images:
-            return f'<img src="/{model.images}" width="100"/>'
-        return "Sem imagem"
-
-    column_formatters = {"images": _format_image}
