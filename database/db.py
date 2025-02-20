b265bfe2a7530e2a383640096e33afbcda5ad57f
jonatas1n <jonatasgeek@gmail.com>
2025-02-20 01:33:04 -0300

    chore: update db file

--- database/db.py
+++ database/db.py
@@ -2,28 +2,31 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 from .base import Base
-import mysql.connector
 from experiences.models import *
 from projects.models import *
 from skills.models import *
 import os
 
-DATABASE_HOST = os.getenv("DATABASE_HOST", "jonatas1n-desktop")
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "portfolio")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "")
+DATABASE_USER = os.getenv("DATABASE_USER", "")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "16604")
 
-DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={
-        "ssl_disabled": False
+        "ssl": {
+            "ca": "ca.pem",
+            "mode": "REQUIRED"
+        }
     }
 )
 
 with engine.connect() as conn:
-    conn.execute(text("CREATE DATABASE IF NOT EXISTS portfolio"))
+    conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}"))
     conn.commit()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
