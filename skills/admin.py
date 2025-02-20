206613c24db59505633c3f1541a09959d5f6000d
jonatas1n <jonatasgeek@gmail.com>
2025-02-20 18:13:03 -0300

    chore: add strip method to strings

--- skills/admin.py
+++ skills/admin.py
@@ -6,6 +6,13 @@ class SkillAdmin(ModelView, model=Skills):
     edit_template = "custom_edit.html"
     create_template = "custom_create.html"
 
+    def on_model_change(self, data, model, is_created, request):
+        if "title" in data:
+            data["title"] = data["title"].strip()
+        if "skill_type" in data:
+            data["skill_type"] = data["skill_type"].strip()
+        return super().on_model_change(data, model, is_created, request)
+
     column_list = [Skills.id, Skills.title, Skills.description]
     column_searchable_list = [Skills.title]
     column_sortable_list = [Skills.id, Skills.title]
